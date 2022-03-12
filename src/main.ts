33a849271c6dc6707e6ceb7f93d6987e5539438e
Kevin Galligan <kgalligan@gmail.com>
2022-03-12 11:02:58 -0500

    check result

--- src/main.ts
+++ src/main.ts
@@ -3,40 +3,51 @@ import * as github from '@actions/github'
 import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
-    try {
-        await fixPrBase()
-    } catch (error) {
-        if (error instanceof Error) core.setFailed(error.message)
-    }
+  try {
+    await fixPrBase()
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
 
-const parsePullRequestId = (githubRef:string) => {
-    const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
-    if (!result) throw new Error("Reference not found.");
-    const [, pullRequestId] = result;
-    return pullRequestId;
-};
+const parsePullRequestId = (githubRef: string) => {
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef)
+  if (!result) throw new Error('Reference not found.')
+  const [, pullRequestId] = result
+  return pullRequestId
+}
 
-async function fixPrBase(){
-    const owner = github.context.repo.owner
-    const repo = github.context.repo.repo
-    const pullRequestId = parsePullRequestId(github.context.ref);
-    const faktorySecretKey: string = core.getInput('FAKTORY_SECRET_KEY')
+async function fixPrBase() {
+  const owner = github.context.repo.owner
+  const repo = github.context.repo.repo
+  const pullRequestId = parsePullRequestId(github.context.ref)
+  const faktorySecretKey: string = core.getInput('FAKTORY_SECRET_KEY')
 
-    await fetch(`https://api.touchlab.dev/gh/movePrBase/${owner}/${repo}/${pullRequestId}`, {
-        headers: {
-            'FAKTORY_SECRET_KEY': faktorySecretKey
-        }
+  await fetch(
+    `https://api.touchlab.dev/gh/movePrBase/${owner}/${repo}/${pullRequestId}`,
+    {
+      headers: {
+        FAKTORY_SECRET_KEY: faktorySecretKey
+      }
+    }
+  )
+    .then(response => {
+      if (response.ok) {
+        throw new Error('Failed fix pr')
+      } else {
+        return response
+      }
     })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => {
-            if (error instanceof Error) {
-                core.setFailed(error.message)
-            } else {
-                core.setFailed(error)
-            }
-        })
+
+    .then(async response => response.json())
+    .then(data => console.log(data))
+    .catch(error => {
+      if (error instanceof Error) {
+        core.setFailed(error.message)
+      } else {
+        core.setFailed(error)
+      }
+    })
 }
 
 run()
